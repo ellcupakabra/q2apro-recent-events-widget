9ca09940afb456a714f991c81a12987a7ce2d7b8
ellcupakabra <ellcupakabra@outlook.cz>
2014-12-13 15:46:17 +0000

    Create q2apro-recent-events-widget-lang-cs.php
    translation into Czech language ..
    from ellcupakabra
    Enjoy you: D

---
+++ q2apro-recent-events-widget-lang-cs.php
@@ -0,0 +1,26 @@
+<?php
+/*
+	Question2Answer Plugin: Recent Events Widget
+	License: http://www.gnu.org/licenses/gpl.html
+*/
+
+	return array(
+		'recent_events' => 'Nedávné události', // DE: Aktuelle Events
+		'question' => 'Otázka', // DE: Frage
+		'new_question' => 'Nová otázka', // DE: Neue Frage
+		'new_question_abbr' => 'Q', // DE: F
+		'new_answer' => 'Nová odpověď', // DE: Neue Antwort
+		'new_answer_abbr' => 'A', // DE: A
+		'new_comment' => 'Nový komentář', // DE: Neuer Kommentar
+		'new_comment_abbr' => 'C', // DE: K
+		'new_bestanswer' => 'Nejlepší odpověď', // DE: Beste Antwort
+		'new_bestanswer_abbr' => 'B', // DE: B
+		'new_by' => 'Od', // new x BY member x, DE: von
+		'anonymous' => 'Anonymní', // new x BY member x, DE: von
+		'hour_indic' => 'h', // will appear behind time, e.g. 06:45h
+	);
+	
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
